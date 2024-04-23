2607ea0997fe1191a0df2f35dbdd53bce9bd8e54
JingpengZhang <jingpeng_zhang@foxmail.com>
2024-04-23 14:03:52 +0800

    feat: 调整文件树宽度

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,10 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      view = {
+        width = 40,
+      },
+    })
   end,
 }
